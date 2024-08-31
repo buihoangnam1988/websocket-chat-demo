1d536c0d6f5e751e6bda529cda8bdaa17b9d2cc9
buihoangnam1988 <buihoangnam1988@gmail.com>
2024-08-31 11:26:10 +0900

    WSClient: Implement simple login UI

--- webSocketClient/src/index.js
+++ webSocketClient/src/index.js
@@ -1,13 +1,20 @@
 import ReactDOM from 'react-dom/client'; 
 import React, {Component} from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
-import './index.css';
+import {Card, Avatar, Input, Typography} from 'antd';
+import 'antd/dist/reset.css'; // `antd/dist/antd.css` does not work, Ref: https://stackoverflow.com/a/76896997/9002449
+
+const { Search } = Input;
 
 const client = new W3CWebSocket('ws://localhost:8002');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+    const [userName, setUserName] = React.useState('');
+    //cosnt [messages, setMessages] = React.useState([]);
+
     React.useEffect(() => {
         client.onopen = () => {
             console.log('WebSocket Client Connected');
@@ -26,9 +33,25 @@ const App = () => {
     }
 
     return (
-        <div>
-            <h1>Hello, World!</h1>
-            <button onClick={() => handleBtnClick("Hello")}>Send Message</button>
+        <div className='main'>
+            {   isLoggedIn 
+                ? <button onClick={() => handleBtnClick("Hello")}>Send Message</button>
+                : <>
+                    <div style={{padding: "200px 40px"}}>
+                        <Search
+                            placeholder='Enter Username'
+                            enterButton='Login'
+                            size='large'
+                            onSearch={value => {
+                                setUserName(value);
+                                setIsLoggedIn(true);
+                            }}
+                        />
+
+                    </div>
+                </>
+            }
+            
         </div>
     );
 }
